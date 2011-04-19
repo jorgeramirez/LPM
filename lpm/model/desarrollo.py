0ba38bd1e0f2282088bc57e75c1da4a162868129
Jorge Ramirez <jorgeramirez1990@gmail.com>
2011-04-19 19:06:08 -0400

    Actualizacion del modulo lpm.model.desarrollo
    He agregado mas clases. Aun faltan algunas clases
    que guardan relacion con este modulo

--- lpm/model/desarrollo.py
+++ lpm/model/desarrollo.py
@@ -4,22 +4,35 @@
 Este módulo contiene las clases del modelo referentes
 al B{Módulo de Desarrollo}
 
+Ejemplo de utilización de L{PropiedadItem}, L{RelacionPorItem} y L{Relacion}::
+    pi = PropiedadItem()
+    rpi = RelacionPorItem()
+    rpi.relacion = Relacion()
+    pi.relaciones.append(rpi)
+
+Iterar sobre los objetos Relacion via RelacionPorItem::
+    for rpi in pi.relaciones:
+        print rpi.revisar
+        print rpi.relacion
+
 @authors:
     - U{Carlos Bellino<mailto:carlosbellino@gmail.com>}
     - U{Nahuel Hernández<mailto:nahuel.11990@gmail.com>}
-    - U{Jorge Ramírez<mailto:jorgeramirez1990@gmail.com}
+    - U{Jorge Ramírez<mailto:jorgeramirez1990@gmail.com>}
 
 @since: 1.0
 """
 
 from sqlalchemy import ForeignKey, Column
-from sqlalchemy.types import Integer, String
+from sqlalchemy.types import Integer, String, Boolean, LargeBinary
 from sqlalchemy.orm import relation
 
 from lpm.model import DeclarativeBase, DBSession
 
 
-__all__ = ['Item', 'PropiedadItem', 'TipoItem', 'AtributosPorTipoItem']
+__all__ = ['Item', 'PropiedadItem', 'TipoItem', 'AtributosPorTipoItem',
+           'RelacionPorItem', 'Relacion', 'AtributosDeItems',
+           'ArchivosExternos', 'ArchivosPorItem']
 
 
 class Item(DeclarativeBase):
@@ -55,6 +68,10 @@ class PropiedadItem(DeclarativeBase):
     prioridad = Column(Integer, nullable=False)
     estado = Column(String(20), nullable=False)
     id_item_actual = Column(Integer, ForeignKey('tbl_item.id_item'))
+    
+    #{ Relaciones
+    relaciones = relation('RelacionPorItem', backref="propiedad_item")
+    archivos = relation('ArchivosPorItem', backref="propiedad_item")
     #}
 
 
@@ -84,6 +101,7 @@ class AtributosPorTipoItem(DeclarativeBase):
     Clase que define que atributos posee un determinado
     tipo de ítem.
     """
+    __tablename__ = 'tbl_atributos_por_tipo_item'
     
     #{ Columnas
     id_atributos_por_tipo_item = Column(Integer, autoincrement=True, 
@@ -92,5 +110,102 @@ class AtributosPorTipoItem(DeclarativeBase):
     tipo = Column(String(32), nullable=False)
     valor_por_defecto = Column(String(32), nullable=True)
     id_tipo_item = Column(Integer, ForeignKey('tbl_tipo_item.id_tipo_item'))
+    
+    #{ Relaciones
+    atributos = relation("AtributosDeItems", backref="tipo_item")
     #}
-   
+
+
+class RelacionPorItem(DeclarativeBase):
+    """
+    Clase que asocia los ítems con sus relaciones
+    """
+    __tablename__ = 'tbl_relacion_por_item'
+    
+    #{Columnas
+    id_relacion_por_item = Column(Integer, autoincrement=True, 
+                                  primary_key=True)
+    revisar = Column(Boolean, nullable=False)
+    id_propiedad_item = Column(Integer, 
+                            ForeignKey('tbl_propiedad_item.id_propiedad_item'))
+    id_relacion = Column(Integer, ForeignKey('tbl_relacion.id_relacion'))
+    
+    #{ Relaciones
+    relacion = relation("Relacion", backref="relacion_item")
+    #}
+
+
+class Relacion(DeclarativeBase):
+    """
+    Clase que define una relación entre ítems.
+    """
+    __tablename__ = 'tbl_relacion'
+    
+    #{ Columnas
+    id_relacion = Column(Integer, autoincrement=True, primary_key=True)
+    tipo = Column(String(45), nullable=False)
+    id_anterior = Column(Integer, ForeignKey('tbl_item.id_item'))
+    id_posterior = Column(Integer, ForeignKey('tbl_item.id_item'))
+    
+    #{ Relaciones
+    anterior = relation("Item", backref=backref("relacion_anterior", uselist=False))
+    posterior = relation("Item", backref=backref("relacion_posterior", uselist=False))
+    #}
+    
+
+class AtributosDeItems(DeclarativeBase):
+    """
+    Clase que define el valor para un atributo del item. Dicho atributo
+    forma parte del conjunto de atributos del L{TipoItem}.
+    """
+    __tablename__ = 'tbl_atributos_de_items'
+    
+    #{ Columnas
+    id_atributos_de_items = Column(Integer, 
+                                   autoincrement=True, primary_key=True)
+    id_atributos_por_tipo_item = Column(Integer, 
+        ForeignKey('tbl_atributos_por_tipo_item.id_atributos_por_tipo_item'))
+    valor = Column(String(200), nullable=False)
+    #}
+
+
+class ArchivosExternos(DeclarativeBase):
+    """
+    Clase que representa un archivo externo que puede ser adjuntado a un
+    L{Item}
+    """
+    __tablename__ = 'tbl_archivos_externos'
+    
+    #{ Columnas
+    id_archivo_externo = Column(Integer, autoincrement=True, primary_key=True)
+    archivo = Column(LargeBinary(2048), nullable=False)
+    nombre_archivo = Column(String(50), nullable=False)
+    #}
+
+
+class ArchivosPorItem(DeclarativeBase):
+    """
+    Clase que asocia un ítem con sus archivos externos
+    """
+    __tablename__ = 'tbl_archivos_por_item'
+    
+    #{ Columnas
+    id_archivos_por_item = Column(Integer, autoincrement=True, primary_key=True)
+    id_propiedad_item = Column(Integer, 
+        ForeignKey('tbl_propiedad_item.id_propiedad_item'))  #parte izquierda de la relación
+    id_archivo_externo = Column(Integer,
+        ForeignKey('tbl_archivos_externos.id_archivo_externo')) #parte derecha de la relación
+    
+    #{ Relaciones
+    archivo = relation("ArchivosExternos", backref="archivos_assoc")
+    #} 
+
+
+
+
+    
+
+    
+    
+    
+    
