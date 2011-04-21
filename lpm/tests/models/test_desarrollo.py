f9721e02b167cce1bf40ee58fed6b9a72f84b79d
Jorge Ramirez <jorgeramirez1990@gmail.com>
2011-04-21 19:11:59 -0400

    Definicion de modulos de prueba para lpm.model
    El modulo que se utiliza para las pruebas es nose
    la documentacion del TG para este modulo se
    encuentra en
    http://turbogears.org/2.1/docs/main/Testing/index.html

---
+++ lpm/tests/models/test_desarrollo.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Módulo de prueba para lpm.model.desarrollo
+
+@authors:
+    - U{Carlos Bellino<mailto:carlosbellino@gmail.com>}
+    - U{Nahuel Hernández<mailto:nahuel.11990@gmail.com>}
+    - U{Jorge Ramírez<mailto:jorgeramirez1990@gmail.com>}
+
+@since: 1.0 
+"""
+from nose.tools import eq_
+
+from lpm import model
+from lpm.tests.models import ModelTest
